50ba6dcb25f27dc52ec7d2ef088fb81e08292e9c
Petar <petar.slovic@gmail.com>
2013-09-06 15:49:19 +0200

    Nothing.

--- js/directives.js
+++ js/directives.js
@@ -2,10 +2,4 @@
 
 /* Directives */
 
-
-angular.module('lhe.directives', []).
-  directive('appVersion', ['version', function(version) {
-    return function(scope, elm, attrs) {
-      elm.text(version);
-    };
-  }]);
+angular.module('lhe.directives', []);
