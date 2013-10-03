a9ca626cec6b0928e8c531b8d61c491ebeec419b
Petar <petar.slovic@gmail.com>
2013-10-03 03:40:26 +0200

    Search now searches only first level folders and files

--- includes/getFirstLevel.php
+++ includes/getFirstLevel.php
@@ -4,9 +4,9 @@ function scandirSorted($path) {
     $sortedDataF = array();$sortedDataD = array();
     foreach(array_diff(scandir($path, 1), array('.', '..')) as $file) {
         if(is_file($path . $file)) {
-            array_unshift($sortedDataF, array('name'=>$file, 'type'=>'file', 'path'=>'../../'.$file));
+            array_unshift($sortedDataF, array('name'=>$file, 'type'=>'file', 'path'=>'../../'.$file, 'level'=>0));
         } else {
-            array_unshift($sortedDataD, array('name'=>$file, 'type'=>'dir', 'path'=>'../../'.$file));
+            array_unshift($sortedDataD, array('name'=>$file, 'type'=>'dir', 'path'=>'../../'.$file, 'level'=>0));
         }
     }
     return array_merge($sortedDataD, $sortedDataF);
--- js/filters.js
+++ js/filters.js
@@ -3,8 +3,17 @@
 /* Filters */
 
 angular.module('lhe.filters', []).
-  filter('interpolate', ['version', function(version) {
-    return function(text) {
-      return String(text).replace(/\%VERSION\%/mg, version);
+  filter('firstLevelName', [function() {
+    return function(entries, search) {
+    	if(!search) {
+    		return entries;
+    	}
+    	return _.filter(entries, function(entry){
+    		if('level' in entry && entry.level == 0) {
+    			return (new RegExp(search, 'i')).test(entry.name);
+    		}
+    		return true;
+    				
+    	});
     };
   }]);
